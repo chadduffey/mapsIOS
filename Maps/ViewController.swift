7d7911e396b87bc83e6600ea7c74b079d739fe5e
Chad Duffey <chadduffey@hotmail.com>
2015-04-13 17:12:51 +1000

    cowra :)

--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //cowra = 33.834968, 148.693319
+        
+        var lattitude:CLLocationDegrees = -33.834968
+        var longtitude: CLLocationDegrees = 148.693319
+        
+        //difference between one side of screen and other
+        //gives us zoom level
+        var latDelta:CLLocationDegrees = 0.01
+        var lonDelta:CLLocationDegrees = 0.01
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        
+        var location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(lattitude, longtitude)
+        
+        
+        var region: MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        map.setRegion(region, animated: true)
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
